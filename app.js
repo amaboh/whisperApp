0b77ec365233f6cdb45c408779f9c293ed9376ce
amaboh <amabocanon@gmail.com>
2022-04-30 15:51:30 +0100

    updated files with all routes for stories functionalities update, delete, and render single users stories

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const passport = require('passport');
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override') 
 const session = require('express-session');
 const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
 
 // Load config files from config files
-
 dotenv.config({path: './config/config.env'})
 
 // Passport config
@@ -20,14 +20,48 @@ connectDB()
 
 const app = express();
 
-// Loggingg with morgan to see the commands made to the server in the terminal
+// Body parser
+app.use(express.urlencoded({extended: false})) //get data from form 
+app.use(express.json())
 
+// method override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  }))
+
+
+// handlebars helpers
+const {
+        formatDate, 
+        stripTags, 
+        truncate, 
+        editIcon,
+        select
+    } = require('./helpers/hbs')
+
+
+// Loggingg with morgan to see the commands made to the server in the terminal
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 
 // Handlebars
-app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs'}));
+app.engine(
+    '.hbs', 
+    exphbs.engine({ helpers: {
+        formatDate,
+        stripTags,
+        truncate,
+        editIcon,
+        select,  
+        },
+        defaultLayout: 'main', 
+        extname: '.hbs'}));
 app.set('view engine', '.hbs');
      
 // sessions
@@ -40,12 +74,15 @@ app.use(session({
 
 )
 
- 
-
-
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+// set gloabal var to access var from anywhere with the templates
+app.use(function(req, res, next) {
+    res.locals.user = req.user || null
+    next()
+})
 
 
 // Static folder for styles/images
@@ -56,7 +93,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))   
 app.use('/stories', require('./routes/stories'))  
-
 
 
 const PORT = process.env.PORT || 3000
--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ router.get('/dashboard', ensureAuth, async (req, res) => {
     try {
         const stories = await Story.find({user: req.user.id}).lean()
         res.render('dashboard',{
-            name: req.user.firsName,
+            name: req.user.firstName,
             stories
         })
     } catch (error) {
